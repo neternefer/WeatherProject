2651c521efab54020bba7697f12d5c6c869a1153
neternefer <alkarinqe@yahoo.com>
2020-04-16 21:16:04 +0200

    Add first draft of styling

--- public/app.js
+++ public/app.js
@@ -1,67 +1,77 @@
-// Personal API Key for OpenWeatherMap API
-const apiKey = 'bf0eea23c1b653273e0023f6a8f41df6';
-const basicUrl = `api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}`;
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = 'bf0eea23c1b653273e0023f6a8f41df6';
+    const basicUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
-// Event listener to add function to existing HTML DOM element
-document.getElementById('generate').addEventListener('click', getData);
-/* Function called by event listener */
-const getData = () => {
-    const zipCode = document.getElementById('zip').value;
-    const userInput = document.getElementById('feelings').value;
-    getWeather(basicUrl, zipCode, apiKey)
-    .then((res) => {
-        postWeather('/addWeather', {date: res.date, temp: res.temp, userInput: res.userInput});
-    })
-    .then(() => updateUI())
-    .catch((e) => console.log('Error: ', e));
-};
-/* Function to GET Web API Data*/
-const getWeather = async (url, zip, key) => {
-    const response = await fetch(basicUrl);
-    try {
-        const data = await response.json();
-        console.log(data);
-        return data;
-    } catch (e){
-        console.log('Error: ', e);
-    }
-};
+    const getData = (e) => {
+        e.preventDefault();
+        const date = new Date();
+        const zipCode = document.getElementById('zip').value;
+        const content = document.getElementById('feelings').value;
+        console.log(content)
+        getWeather(basicUrl, zipCode, apiKey)
+        .then((res) => {
+            postWeather('/add', {date: date,
+                temp: res.main.temp,
+                content: content});
+        })
+        .then(() => updateUI())
+        .catch((e) => console.log('Error: ', e));
+    };
 
-/* Function to POST data */
-const postWeather = async (url, dataObj) => {
-    const request = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(
-            {
-                date: dataObj.date,
-                temp: dataObj.temp,
-                userInput: dataObj.userInput
-            }
-        )
-    });
-    try {
-        const data = await request.json();
-        return data;
-    } catch (e) {
-        console.log('Error: ', e)
-    }
-};
+    document.getElementById('generate').addEventListener('click', getData);
 
-
-/* Function to GET Project Data */
-const updateUI = async () => {
-    const request = await fetch('/weather');
-    try {
-        const allInfo = await request.json();
-        const elements = getElementsByClassName('uiData');
-        for(element of elements){
-            element.innerHTML= allInfo[0].element.id;
+    const getWeather = async (url, zip, key) => {
+        const response = await fetch(url + zip + '&appid=' + key);
+        try {
+            const data = await response.json();
+            return data;
+        } catch (e){
+            console.log('Error: ', e);
         }
-    } catch(e) {
-        console.log('Error: ', e);
-    }
-};
+    };
+
+    const postWeather = async (url, dataObj={}) => {
+        const response = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+           // Body data type must match "Content-Type" header
+            body: JSON.stringify(dataObj),
+          });
+
+            try {
+              const newData = await response.json();
+              return newData;
+            }catch(error) {
+            console.log("error", error);
+            }
+        }
+
+    const updateUI = async () => {
+        const request = await fetch('/all');
+        try {
+            const allInfo = await request.json();
+            console.log(allInfo)
+            const elements = document.querySelectorAll('.uiData');
+            elements.forEach((e) => {
+                let key = e.id;
+                e.innerHTML = allInfo[key];
+            })
+            showDisplay();
+        } catch(e) {
+            console.log('Error: ', e);
+        }
+    };
+
+    const showDisplay = () => {
+        //show or hide side menu
+        const display = document.querySelector('.display');
+        if(display.style.width === '' || display.style.width === '0px') {
+            display.style.width = '100vw';
+        } else {
+            display.style.width = '0px';
+        }
+    };
+});
--- server.js
+++ server.js
@@ -11,21 +11,23 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000...');
-});
 
 // Initialize all route with a callback function
-app.get('/weather', (req, res) => {
+app.get('/all', (req, res) => {
     res.send(projectData);
 });
-app.post('/addWeather', (req, res) => {
-    projectData['temp'] = req.body.temp;
+app.post('/add', (req, res) => {
     projectData['date'] = req.body.date;
-    projectData['userInput'] = req.body.userInput;
+    projectData['temp'] = req.body.temp;
+    projectData['content'] = req.body.content;
     res.send(projectData);
 });
 // Callback function to complete GET '/all'
 
 // Post Route
+
+
+app.listen(5000, () => {
+    console.log('Listening on port 5000...');
+});
 
